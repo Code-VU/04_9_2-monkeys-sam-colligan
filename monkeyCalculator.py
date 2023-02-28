f7bb03033b2d1c23522dd01ffa83a50a45f876b2
Sam Colligan <Sam.Colligan@vu.com>
2023-02-28 09:35:56 -0600

    made changes

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,11 +4,19 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    if monkey_one.lower() == "y" and monkey_two.lower () == "y" :
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one.lower() =="n" and monkey_two.lower() == "n" :
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
+
+        
+if __name__ == "__main__":
+    calculateTime()
+
     # end assignment
 
 
 ## If you want to test locally run > python monkeyCalculator.py
 
-if __name__ == "__main__":
-    calculateTime()
-    
